fc556eceabae256293e8982a96725cdd4ec27349
meu241089 <giangimeu89@gmail.com>
2025-04-04 10:14:14 +0200

    avanti coi tutorials

--- Tutorial/TypeCasting.c
+++ Tutorial/TypeCasting.c
@@ -4,6 +4,7 @@
 
 int main()
 {
+
     int a;
     float b, q;
 
@@ -20,6 +21,33 @@ int main()
     /*explicity type casting*/
     q = (float)a / b;
     printf("%f\n", q);
+
+    int c, d, r;
+    c = 15, d = 2;
+
+    r = c / d;
+
+    printf("%d\n", r);
+    printf("%f\n", r);
+
+    /*
+        char = 8 bits -> 256 combinations
+        long long int = 64 bit -> 2^64 combinations
+        int, float, double = 32 bit -> 2^32 combinations
+    */
+
+    char e;
+    int f;
+
+    f = 100;
+    e = f;
+
+    printf("%d\n", e); // printf 100 beacuse we are in the rage of char
+
+    f = 300;
+    e = f;
+
+    printf("%d\n", e); // print 44 beacuse 300 - 256 = 44, we lost information
 
     return 0;
 }
